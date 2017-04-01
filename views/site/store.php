739a8437947282ae03fbdedbe1ac2ca98199d802
Muhammad Gilang Januar <mgilangjanuar@gmail.com>
2017-04-01 13:08:20 +0700

    add Filter by City text on store page

--- views/site/store.php
+++ views/site/store.php
@@ -33,6 +33,7 @@ $this->registerCss("
 
     <br /><br /><br />
     <div class="container text-center">
+        Filter by City: &nbsp; &nbsp;
         <?= Html::a('All &nbsp <i class="fa fa-times"></i>', ['/site/store'], ['style' => 'margin:3px', 'class' => ($model->state ? 'btn btn-secondary' : 'btn btn-alternate')]) ?>
         <?php foreach ($states as $state): ?>
             <?= Html::a($state, ['/site/store', 'SearchStoreForm[state]' => $state], ['style' => 'margin:3px', 'class' => ($model->state == $state ? 'btn btn-alternate' : 'btn btn-secondary')]) ?>
